eaa51e837f7d0e8c69b5753ed815e321053e9627
LilFatFrank <karan.parsnani@ymail.com>
2019-12-06 01:00:03 +0530

    anagrams and sentence case

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
 
 module.exports = anagrams;
+
+/* solution 1:
+function anagrams(stringA, stringB) {
+
+    const objectA = buildCharMaps(stringA);
+    const objectB = buildCharMaps(stringB);
+
+    if (((Object.keys(objectA)).length === (Object.keys(objectB)).length)) {
+        for (let i in objectA) {
+            if (objectA.hasOwnProperty(i)) {
+                if (objectA[i] !== objectB[i]) {
+                    return false;
+                }
+            }
+        }
+        for (let i in objectB) {
+            if (objectB.hasOwnProperty(i)) {
+                if (objectA[i] !== objectB[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+function buildCharMaps(string) {
+
+    const stringObj = {};
+
+    for ( const i of string.replace(/[^\w]/g, '').toLowerCase()) {
+        stringObj[i] = stringObj[i] + 1 || 1;
+    }
+
+    return stringObj;
+
+}
+*/
