34f1d2de08bcf3c842cb3ad100260613443cb6cc
LilFatFrank <karan.parsnani@ymail.com>
2019-12-10 23:24:01 +0530

    Find the vowels exercise

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+
+}
 
 module.exports = vowels;
+
+
+/* solution 1
+let count = 0;
+    const vowels = ['a','e','i','o','u'];
+
+    for (const i of str.toLowerCase()) {
+        if (vowels.includes(i)) {
+            count+=1;
+        }
+    }
+
+    return count;
+ */
